1ffde0e5579878191e860e72f8d30aea1353ab69
Blackdeniel360 <saurirenren@gmail.com>
2026-03-29 23:07:40 +0800

    without banner

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,15 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-      <h1>helloooooooo</h1>
-      <Row title="Netflix Originals" />
-      <Row title="Trending Now" />
-      
-      
+
+      <Row
+        title="NUTFLIX"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+
+      <Row
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+      />
+
+      <Row
+        title="Top Rated"
+        fetchUrl={requests.fetchTopRated}
+      />
+
+      <Row
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+      />
+
+      <Row
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+
+      <Row
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+
+      <Row
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+
     </div>
   );
 }
